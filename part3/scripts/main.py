492a2c67e020ab6d1e38f7e073835a193f7e05b9
kirphone <bulanovks@mail.ru>
2021-02-18 00:57:10 +0300

    Генератор конкретных продуктов

--- part3/scripts/main.py
+++ part3/scripts/main.py
@@ -11,10 +11,10 @@ load_dotenv()
 conn = psycopg2.connect(dbname='studs', user=os.getenv('DB_USER_NAME'), 
                         password=os.getenv('DB_PASSWORD'), host='localhost', port = "4757")
 cursor = conn.cursor()
-#create_account(200, cursor)
-#create_shops(cursor)
+conn.autocommit = True
+create_accounts(200, cursor)
+create_shops(cursor)
 create_products(200, cursor)
 
-conn.commit()
 cursor.close()
 conn.close()
