d83c714caea69e6fc0caa833dd9c3f2a65833564
kirphone <bulanovks@mail.ru>
2021-02-17 17:52:36 +0300

    Удаление данных и таблиц

---
+++ part3/delete_all_records.sql
@@ -0,0 +1,8 @@
+delete from "order";
+delete from discount_card;
+delete from shop;
+delete from product;
+delete from product_category;
+delete from shop_company;
+delete from status_order;
+delete from account;
---
+++ part3/drop_db_objects.sql
@@ -0,0 +1,12 @@
+drop table if exists "order",
+                     discount_card,
+                     shop,
+                     product,
+                     product_category,
+                     shop_company,
+                     status_order,
+                     account,
+                     product_shop
+                     message,
+                     review,
+                     order_product;
