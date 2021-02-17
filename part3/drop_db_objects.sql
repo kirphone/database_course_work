39badb0a7f721b0c04c6240a0bfbda51d81858cb
kirphone <bulanovks@mail.ru>
2021-02-17 19:06:23 +0300

    Удаление функций

--- part3/drop_db_objects.sql
+++ part3/drop_db_objects.sql
@@ -15,3 +15,30 @@ drop function if exists Check_Status_Order();
 drop function if exists CHECK_CHANGE_STATUS_ORDER();
 drop function if exists Check_Started_Status_Order();
 drop function if exists CHECK_CORRECT_SENDER();
+drop function if exists Create_User(
+    name_in varchar(63),
+    phone_in varchar(31),
+    email_in varchar(63),
+    password_in varchar(63)
+    );
+drop function if exists select_product_shop(
+    shop_id_in int
+);
+drop function if exists create_order(
+    shop_id_in int,
+    address_lat_in double precision,
+    address_lng_in double precision,
+    customer_id_in int
+);
+drop function if exists add_order_product(
+    product_id_in int,
+    count int,
+    price_in int,
+    need_confirm_in boolean,
+    order_id_in int
+);
+drop function if exists create_message(
+    sender_id_in int,
+    order_id_in int,
+    message text
+);
